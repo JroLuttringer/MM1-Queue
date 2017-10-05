76369aa0470df5a2253d063a3091a9bfc2e50ce8
Jean-Romain Luttringer <jrluttringer@etu.unistra.fr>
2017-10-05 12:28:55 +0200

    Fonctionnel

--- src/Ech.java
+++ src/Ech.java
@@ -24,36 +24,62 @@ public class Ech {
 	
 	public void traiter_premier_event(){
 		Evt evt_courant = echeancier.pop();
-		System.out.println(evt_courant.toString());
+		//System.out.println("=============");
+		System.out.println("Traitement de "+evt_courant.toString());
 		if( evt_courant.is_depart() == false){			
 			// calcul arrivée client suivant
 			double date_suivante = evt_courant.get_date() + Utile.loi_exp(lambda);
-			if( date_suivante <= date_maximale){
-				Evt nvl_evt = new Evt(false, date_suivante, dernier_id +1);
-				dernier_id++;
-				inserer_evt(nvl_evt);	
+			//System.out.println("Date d'arrivée suivante : "+date_suivante);
+			double date_depart;
+			
+			
+			
+			if(echeancier.isEmpty()) {
+				date_depart = evt_courant.get_date() + Utile.loi_exp(mu);
 			}
+			else {
+				//System.out.println("Depart max avant : "+ depart_max);
+				date_depart = depart_max + Utile.loi_exp(mu);
+			}
+			
+			//System.out.println("Date de depart: "+date_depart);
 			// calcul départ client courant
-			double date_depart = depart_max + Utile.loi_exp(mu);
 			depart_max = date_depart;
+			//System.out.println("Date depart :" + date_depart);
+			//System.out.println("Depat max après : "+depart_max);
 			evt_courant.set_depart(true);
 			evt_courant.set_date(date_depart);
 			inserer_evt(evt_courant);
+			if( date_suivante <= date_maximale){
+				Evt nvl_evt = new Evt(false, date_suivante, ++dernier_id);
+				inserer_evt(nvl_evt);	
+			}
 		}  // fin event arrivee
 		else {
 			
 		}
 		
+		//System.out.println("Fin : date départ max : " + depart_max);
+		//System.out.println("=============");
+		
 	}
 	
 	private void inserer_evt(Evt evt){
-		int i =0;
-		while(i < echeancier.size()){ /// ->>>> trouver date max et inserer après
-			
+		int i = 0;
+
+		while(i < echeancier.size() && (echeancier.get(i).get_date() < evt.get_date() )){ /// ->>>> trouver date max et inserer après
 			i++;	
+			//System.out.println(i);
 		}
-		echeancier.add(i, evt);		
-		System.out.println(i);
+		
+		echeancier.add(i, evt);	
+		//System.out.println(echeancier.get(i).get_date());
+
+		i=0;
+		/*while(i < echeancier.size()){ /// ->>>> trouver date max et inserer après
+			System.out.println("File["+i+"] : "+echeancier.get(i).toString());
+			i++;
+		}*/
 	}
 	
 	public boolean est_vide(){
