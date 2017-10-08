716fb4542d83eaf8a423989cc5b0d7fff217b2a1
Jean-Romain Luttringer <jrluttringer@etu.unistra.fr>
2017-10-09 01:36:29 +0200

    oubli dans l'autre commit

--- src/Ech.java
+++ src/Ech.java
@@ -1,14 +1,15 @@
 import java.util.LinkedList;
 
 public class Ech {
-	private LinkedList<Evt> file;
+
+	private LinkedList<Evt> file; // client en attente
 	private double lambda;
 	private double mu;
-	private double date_maximale;
-	private double depart_max;
-	private int dernier_id;
-	private double arrivee_suivante;
-	private Stats s;
+	private double date_maximale; // borne temporelle de la simulation
+	private double depart_max;	// départ le plus tardif calculé
+	private int dernier_id;	// compteur de client
+	private double arrivee_suivante; // date de la prochaine arrivee
+	private Stats s; // Pour calcul des statistiques
 
 	public Ech(double lambda, double mu, double date_max) {
 		this.lambda = lambda;
@@ -16,48 +17,83 @@ public class Ech {
 		this.s = s;
 		date_maximale = date_max;
 
-		depart_max = 0;
 		dernier_id = 0;
 		s = new Stats(lambda, mu, date_maximale);
-		arrivee_suivante = 0;
+		arrivee_suivante = 0; // init de l'echéancier avec une arrivee à t=0
+		depart_max = 1;
 		file = new LinkedList<Evt>();
-		file.add(new Evt(false, 0, -1, 0));
+		//file.add(new Evt(false, 0, -1, 0)); Pour ancienne implémentation
 	}
 
 	public boolean est_vide(){
 		return arrivee_suivante > date_maximale && file.isEmpty();
 	}
 
-	public void demarrer_xp() {
-		/* Récupération du prochain evenement à traiter et recup des info à afficher */
+	public void traiter_arrivee(double date, int verbose){
+		if(verbose==1){
+			System.out.format("Date= %f Arrive client #%d \n",
+				arrivee_suivante, dernier_id);
+		}
+
+		double depart;
+		if(file.isEmpty()) { // file vide -> le client n'attend pas
+			s.incrementer_clients_sans_attente();
+			depart = arrivee_suivante + Utile.loi_exp(mu);
+		} else {
+			depart = depart_max + Utile.loi_exp(mu);
+		}
+
+		// FIFO -> prochain départ en fin de file
+		file.addLast(new Evt(true, arrivee_suivante, depart, ++dernier_id));
+		arrivee_suivante += Utile.loi_exp(lambda);
+
+		// Si prochaine arrivee trop lointaine, on ne traite plus que les départs
+		if(arrivee_suivante > date_maximale)
+			arrivee_suivante = Double.POSITIVE_INFINITY;
+
+		depart_max = depart;
+	}
+
+
+	public void traiter_depart(int verbose){
+		// Mise à jour des statistiques
+		Evt e = file.pop();
+		s.add_temps_sejour(e.get_date_depart() - e.get_date_arrivee());
+
+		if(verbose == 1){
+			System.out.format("Date= %f Depart client #%d arrive à t=%f\n",
+		 		e.get_date_depart(),e.get_id_client(),e.get_date_arrivee());
+		}
+	}
+
+	public void demarrer_xp(int verbose) {
 		double depart_suivant;
+
 		while(!est_vide()){
-			if(file.isEmpty())
+			if(file.isEmpty())	// file vide -> pas de départ à traiter
 				depart_suivant = Double.POSITIVE_INFINITY;
-			else
+			else	// Sinon -> depart suivant = premier départ de la file car FIFO
 			 	depart_suivant = file.getFirst().get_date_depart();
 
-			if(arrivee_suivante < depart_suivant){
-				double depart;
-				if(file.isEmpty()){
-					s.incrementer_clients_sans_attente();
-					depart = arrivee_suivante + Utile.loi_exp(mu);
-				} else {
-					depart = depart_max + Utile.loi_exp(lambda);
-				}
-				depart_max = depart;
-				file.addLast(new Evt(true, arrivee_suivante, depart, ++dernier_id));
-				arrivee_suivante += Utile.loi_exp(lambda);
-				if(arrivee_suivante > date_maximale)
-					arrivee_suivante = Double.POSITIVE_INFINITY;
+			// Traitement de l'evenement suivant
+			if(arrivee_suivante < depart_suivant)
+				traiter_arrivee(arrivee_suivante, verbose);
+			else
+				traiter_depart(verbose);
 
-			} else {
-				Evt e = file.pop();
-				s.add_temps_sejour(e.get_date_depart() - e.get_date_arrivee());
-			}
-		}
-		s.set_clients(dernier_id);
-/*
+			s.set_clients(dernier_id); // Mise à jour nombre de clients total
+		} // fin while !vide
+	}
+
+	public Stats get_resultats(){
+		return s;
+	}
+
+
+
+
+// ===================== ANCIENNE VERSION  ====================//
+ 	public void old(){
 		double depart;
 		while (!file.isEmpty()){
 			Evt premier = file.pop();
@@ -85,22 +121,20 @@ public class Ech {
 		}
 		s.set_clients(dernier_id);
 	}
-*/
+
+
 	public void inserer_evt(Evt e){
 		double date_e = Math.max(e.get_date_depart(), e.get_date_arrivee());
 		if(date_e > depart_max){
 			file.addLast(e);
 		}
 		else{
-			/*int i =0;*/
 			int i =0;
-			while(i<file.size() &&file.get(i).get_date_depart() < date_e)
+			while(i<file.size() && file.get(i).get_date_depart() < date_e)
 				i++;
 			file.add(i, e);
 		}
 	}
 
-	public Stats get_resultats(){
-		return s;
-	}
+
 }
